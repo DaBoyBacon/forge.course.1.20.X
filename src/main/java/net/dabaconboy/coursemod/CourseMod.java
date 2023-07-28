3a8dcfc58a9f902937a4eff4ddc91f727b89822b
MatthewDAgron <mdagron907@ou.edu>
2023-07-27 17:28:04 -0800

    test

--- src/main/java/net/dabaconboy/coursemod/CourseMod.java
+++ src/main/java/net/dabaconboy/coursemod/CourseMod.java
@@ -20,7 +20,7 @@ public class CourseMod {
     public static final String MOD_ID = "coursemod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-
+    private int x = 0;
 
 
     public CourseMod() {
