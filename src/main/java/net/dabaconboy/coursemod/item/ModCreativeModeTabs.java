7df6736c28e49424458aa5b9343e02352645c2cc
MatthewDAgron <mdagron907@ou.edu>
2023-07-30 23:44:41 -0800

    Creative mode tab (cleanup)

--- src/main/java/net/dabaconboy/coursemod/item/ModCreativeModeTabs.java
+++ src/main/java/net/dabaconboy/coursemod/item/ModCreativeModeTabs.java
@@ -15,9 +15,12 @@ public class ModCreativeModeTabs {
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CourseMod.MOD_ID);
 
     public static final RegistryObject<CreativeModeTab> COURSE_TAB = CREATIVE_MODE_TABS.register("course_tab",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SWEATRITE.get())).title(Component.translatable("creativetab.course_tab")).displayItems((displayParameters, output) -> {
-                output.accept(ModItems.SWEATRITE.get());
-                output.accept(ModBlocks.SWEATRITE_BLOCK.get());
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SWEATRITE
+                    .get()))
+                    .title(Component.translatable("creativetab.course_tab"))
+                    .displayItems((displayParameters, output) -> {
+                        output.accept(ModItems.SWEATRITE.get());
+                        output.accept(ModBlocks.SWEATRITE_BLOCK.get());
 
             }).build());
 
